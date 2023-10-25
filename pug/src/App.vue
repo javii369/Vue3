f87752b291ffed7e1ef33099f5f7411d0744ce56
emirandaballadares <emirandaballadares@outlook.com>
2023-10-25 11:40:20 +0200

    pug

---
+++ pug/src/App.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+Lista(:elementos="juegos")
+</template>
+
+<script>
+import Lista from "./components/Lista.vue";
+export default {
+  components: { Lista },
+  data: () => ({
+    juegos: ["CSGO", "Minecraft", "PUBG"],
+  }),
+};
+</script>
+
+<style>
+.elemento1 {
+  color: blue;
+}
+#elemento2 {
+  color: purple;
+}
+</style>
